67f5a03afc100428090463fada3ba07d0916e787
goncalveshigino <goncalveshigino20@gmail.com>
2022-02-14 09:04:55 +0100

    conceitos basicos

---
+++ network/response.js
@@ -0,0 +1,20 @@
+
+
+exports.success = function(req,res, message, status){
+  res.status(status || 200).send(
+     {
+        error: '',
+        body: message
+     }
+  );
+}
+
+exports.error = function(req,res, message,status, details){
+    console.error('[response error] ' + details);
+    res.status(status || 200).send(
+        {
+            error: message,
+            body: ''
+        }
+    );
+}
--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const response = require('./network/response')
+
 const router = express.Router();
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(router);
 
 router.get('/message', function (req,res){
-   res.send('Lista de mensagem')
+    console.log(req.headers);
+    res.header({
+       "Custom-Header": "Tem o nosso valor personalizado",
+    });
+    response.success(req,res, 'Lista de mensagens');
 });
 
 
 router.post('/message', function (req,res){
-    res.send('Mensagem adicionada')
+  console.log(req.body);
+ if(req.query.error == "ok"){
+   response.error(req,res, 'Error Inesperado', 500, 'Simulacao de erros')
+ }
+ response.success(req,res, 'Criado corretamente', 201)
+  
  });
 
 
-// app.use('/', function(req,res){
-//     res.send('Hello');
-// });
+app.use('/app', express.static('public'));
 
 app.listen(3000);
-console.log('Rodando na porta 3000')
+console.log('Rodando na porta 3000');
+
+
+// app.listen(process.env.PORT, () =>{
+//    console.log(`Rodando na porta ${ process.env.PORT}`)
+// });
